7df67679288dee972d6cb9e92248cc7f799c49ba
Michel Wermelinger <michel@wermelinger.ws>
2013-10-06 20:44:16 +0100

    More comments, more unit tests. Added oriented().

--- digraph.py
+++ digraph.py
@@ -13,36 +13,36 @@ This implementation does *not* aim to provide efficient operations for large gra
 
 The terminology was taken from the Wikipedia pages on [digraphs](http://en.wikipedia.org/wiki/Digraph_(mathematics)).
 
-The latest version of this file can be found on http://michel.wermelinger.ws. 
+The latest version of this file can be found on https://bitbucket.org/mwermelinger/digraph-py.
 
-Graph representation
---------------------
-A simple digraph is represented by a pair `(nodes, arcs)`, where `nodes` is the set of unique node names (e.g. integers or strings) and `arcs` is the set of arcs. Each arc is a pair of node names `(tail, head)`, stating there is a directed arc from the `tail` node to the `head` node.
-
-In the rest of the code and documentation, we use the word graph instead of digraph.
+Digraph representation
+----------------------
+A digraph (short for 'directed graph') is represented by a pair `(nodes, arcs)`, where `nodes` is the *set* of unique node names (e.g. integers or strings) and `arcs` is the *set* of arcs. Each arc is a pair of node names `(tail, head)`, stating there is a directed arc from the `tail` node to the `head` node.
 """
 
 # Example graphs
 # --------------
-# The following graphs will be used as unit tests for functions. 
+# The following digraphs will be used as unit tests for functions. 
 
 # The **empty graph** has no nodes and hence no arcs.
-EMPTY = (set(), set())		# {} is the empty dictionary
+EMPTY = (set(), set())		# {} is the empty dictionary, not the empty set
 
-# The **trivial graph** has a single node and no arcs.
-TRIVIAL = ({1}, set())
+# A **null graph** Nn has n nodes but no edges.
+N1 = ({1}, set())			# also called the trivial graph
+N2 = ({1,2}, set())
 
 # The smallest non-simple graph.
 LOOP = ({1}, {(1,1)})
 
-# A **cycle graph** has nodes connected in a 'round-robin' fashion. 
+# A **cycle graph** Cn has n nodes connected in a 'round-robin' fashion. 
 # Cycle graphs can be drawn as regular shapes: triangle, square, pentagon, hexagon, etc.
 C3 = ({1,2,3}, {(1,2), (2,3), (3,1)})               		
 C4 = ({1,2,3,4}, {(1,2), (2,3), (3,4), (4,1)})      		
 C5 = ({1,2,3,4,5},{(1,2), (2,3), (3,4), (4,5), (5,1)})     
 
-# In a **complete graph**, each node is connected to every other node.
+# In a **complete graph** Kn, each of the n nodes is connected to all other n-1 nodes.
 K2 = ({1,2}, {(1,2), (2,1)})
+K3 = ({1,2,3}, {(1,2), (1,3), (2,1), (2,3), (3,1), (3,2)})
 
 # The **Petersen graph** is a (counter-)example of various graph theory concepts.
 # It is usually drawn as a pentagram connected to a pentagon.
@@ -54,20 +54,19 @@ PETERSEN = ({1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 
 # ### Exercises
 #
-# 1. Define K1 and K3, the complete graphs with one and three nodes, respectively.
-# 2. Define C2, the cycle graph with two nodes.
+# 1. Define K1 and C2.
 
 # Basic functions
 # ---------------
 # These functions make the remaining functions independent of the actual data structure.
 
 def arcs(graph):
-	"""Return the set graph's arcs as a set."""
+	"""Return the set of the graph's arcs"""
 	(nodes, arcs) = graph
 	return arcs
 	
 def nodes(graph):
-	"""Return the graph's nodes as a set."""
+	"""Return the set of the graph's nodes."""
 	(nodes, arcs) = graph
 	return nodes
 	
@@ -82,7 +81,7 @@ def head(arc):
 	return head
 	
 def make_arc(tail, head):
-	"""Return an arc from tail to head node."""
+	"""Create an arc from tail node to head node."""
 	return (tail, head)
 	
 # Arc properties
@@ -112,12 +111,12 @@ assert(not adjacent(1, 3, C4))
 def isolated (node, graph):
 	"""A node is **isolated** if it is not adjacent to any other node."""
 	for node2 in nodes(graph):
-		if adjacent(node, node2):
+		if adjacent(node, node2, graph):
 			return False
 	return True
 	
 # Unit tests for `isolated()`
-assert(isolated(1, TRIVIAL))
+assert(isolated(1, N1))
 assert(not isolated(1, K2))
 
 def indegree(node, graph):
@@ -125,7 +124,7 @@ def indegree(node, graph):
 	degree = 0
 	for arc in arcs(graph):
 		if head(arc) == node:
-			degree += 1
+			degree = degree + 1
 	return degree
 	
 def outdegree(node, graph):
@@ -133,31 +132,28 @@ def outdegree(node, graph):
 	degree = 0
 	for arc in arcs(graph):
 		if tail(arc) == node:
-			degree += 1
+			degree = degree + 1
 	return degree
-
-assert(degree(1, graph) == 1)
-assert(degree(2, graph) == 2)
-assert(degree(1, empty) == 0)
 
 def source(node, graph):
 	"""A node is a **source** if it has no incoming arcs."""
 	return indegree(node, graph) == 0
 	
 # Unit tests for `source()`
-assert(source(1, TRIVIAL))
+assert(source(1, N1))
 assert(not source(3, C4))
 
 def sink(node, graph):
 	"""A node is a **sink** if it has no outgoing arcs."""
 	return outdegree(node, graph) == 0
 
-# Unit tests for sink
-assert(sink(1, TRIVIAL))
+# Unit tests for `sink()`
+assert(sink(1, N1))
 assert(not sink(3, C4))
 
 # ### Exercises
 #
+# 1. Implement `outdegree()` and `sink()` and un-comment its unit tests.
 # 1. Rewrite `adjacent()` to make it more efficient, by searching the arc set only once.
 # 1. Is every node adjacent to itself? Does that depend on the existence of a loop? Look for answers on the internet, modify `adjacent()` if needed, and create further unit tests. 
 # 1. Write another version of `isolated()` using the degree concept. Which is more efficient?
@@ -171,7 +167,7 @@ def size(graph):
 
 # Unit tests for `size()`
 assert(size(EMPTY) == 0)
-assert(size(TRIVIAL) == 0)
+assert(size(N1) == 0)
 assert(size(C3) == 3)
 assert(size(PETERSEN) == 15)
 	
@@ -181,7 +177,7 @@ def order(graph):
 	
 # Unit tests for `order()`
 assert(order(EMPTY) == 0)
-assert(order(TRIVIAL) == 1)
+assert(order(N1) == 1)
 assert(order(C3) == 3)
 assert(order(PETERSEN) == 10)
 
@@ -194,29 +190,49 @@ def simple(graph):
 	
 # Unit tests for `simple()`
 assert(simple(EMPTY))
-assert(simple(TRIVIAL))
+assert(simple(N1))
 assert(simple(PETERSEN))
 assert(not simple(LOOP))
 	
-def balanced(graph):
-	"""A digraph is balanced if every node has the same in and out degrees."""
-	for node in nodes(graph):
-		if indegree(node, graph) != outdegree(node, graph):
+def oriented(graph):
+	"""An **oriented** graph is an undirected graph with a direction imposed on each edge."""
+	for arc in arcs(graph):
+		if not loop(arc) and inverted(arc) in arcs(graph):
 			return False
-	return True
-
-# Unit tests for `balanced()`
-assert(balanced(K2))
+	return True		
+	
+assert(oriented(EMPTY))
+assert(oriented(N1))
+assert(oriented(LOOP))
+assert(not oriented(K2))
 
 def symmetric(graph):
-	"""A digraph is symmetric if for every arc it also has the inverted arc."""
+	"""A digraph is **symmetric** if for every arc it also has the inverted arc."""
 	for arc in arcs(graph):
 		if inverted(arc) not in arcs(graph):
 			return False
 	return True
 	
 # Unit tests for `symmetric()`
+assert(symmetric(EMPTY))
+assert(symmetric(N1))
+assert(symmetric(LOOP))
 assert(symmetric(K2))
+assert(not symmetric(C3))
+
+def balanced(graph):
+	"""A digraph is **balanced** if every node has the same in and out degrees."""
+	for node in nodes(graph):
+		if indegree(node, graph) != outdegree(node, graph):
+			return False
+	return True
+
+# Unit tests for `balanced()`
+assert(balanced(EMPTY))
+assert(balanced(N1))
+assert(balanced(K2))
+assert(balanced(C3))
+assert(not balanced(PETERSEN))
 
 def tournament(graph):
 	"""A tournament is a digraph with exactly one arc for each pair of distinct nodes."""
@@ -236,3 +252,4 @@ def acyclic(graph):
 # ### Exercises
 #
 # 1. Why doesn't `simple()` check for duplicate arcs?
+# 1. Rewrite `simple()` to make it more efficient. Hint: usually the order is smaller than the size. 
