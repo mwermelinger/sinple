23c8b2dd66cc2c76560ca94781acd8b8c954e178
Michel.Wermelinger <michel.wermelinger@open.ac.uk>
2014-09-07 17:09:09 +0100

    Distance() returns infinite if no path. Added add_nodes(), density(), components(), giant_component().

--- sinple.py
+++ sinple.py
@@ -8,7 +8,7 @@ gene regulation, social interactions, the World Wide Web.
 [Network science](http://en.wikipedia.org/wiki/Network_science) is an
 important field to analyse and predict the behaviour of such systems.
 
-SINPLE does *not* enable analysis of large networks;
+SINPLE does *not* support analysis of large networks;
 there are various state-of-the-art libraries for that.
 Instead SINPLE's aims are pedagogical:
 
@@ -31,9 +31,27 @@ The latest version of SINPLE can be found on http://tiny.cc/sinple.
 
 # Coding conventions
 # ------------------
-# Functions named with adjectives or `is_`noun return booleans.
+# Functions named with adjectives or `is_`*noun* return booleans.
 # Functions named with nouns return network entities or metrics.
 # The code passes the [PEP8 style checker](https://github.com/jcrocholl/pep8).
+
+# Exercises
+# ---------
+# SINPLE contains many exercises, of varying type and difficulty.
+# Some exercises are more relevant for introductory Python courses,
+# some for data structures and algorithms courses, others for network courses.
+
+# Exercises aren't ordered by type or difficulty: they appear as soon as they
+# can be solved, using the preceding functions.
+# Exercises at the end of SINPLE require changes throughout the library.
+# Exercises are grouped in bullet lists; there is no heading introducing them.
+#
+# A general, implicit, exercise is to study and understand the code.
+# Some exercises focus on explaining particular functions.
+#
+# Some exercises ask to change existing functions, others to add new code.
+# When writing a new function, think about its name and parameters,
+# any preconditions, and add the corresponding unit tests.
 
 # Network representation and creation
 # -----------------------------------
@@ -75,8 +93,6 @@ def edge(node1, node2):
 
 assert edge(1, 2) == edge(2, 1)
 
-# **EXERCISES:**
-#
 # - Consult the Python documentation about sets.
 #   Why is an edge represented by a frozen (i.e. immutable) set?
 
@@ -97,9 +113,16 @@ assert nodes(null(0)) == set()
 assert edges(null(2)) == set()
 assert nodes(null(2)) == {1, 2}
 
-# **EXERCISES:**
-#
 # - Explain how `null()` works. Why is `set()` used instead of `{}`?
+
+
+def add_nodes(nodeset, graph):
+    """Add a set of nodes to graph to create a new graph."""
+    return (nodes(graph) | nodeset, edges(graph) | set())
+
+assert add_nodes({3, 2, 1}, null(0)) == null(3)
+
+# - What does the `|` operator do? Why is the empty set added?
 
 
 def add_edges(edgeset, graph):
@@ -113,9 +136,7 @@ def add_edges(edgeset, graph):
 
 assert add_edges({edge(1, 2)}, null(0)) == add_edges({edge(2, 1)}, null(2))
 
-# **EXERCISES:**
-#
-# - Explain how `add_edges()` works. Why isn't `edgseset` just named `edges`?
+# - Explain how `add_edges()` works.
 
 
 def delete_edges(edgeset, graph):
@@ -124,17 +145,16 @@ def delete_edges(edgeset, graph):
 
 assert delete_edges({edge(1, 2)}, add_edges({edge(2, 1)}, null(0))) == null(2)
 
-# **EXERCISES:**
-#
 # - Write a function to remove a set of nodes from a graph.
-# - Write a function `subgraph(nodeset, graph)` that returns the **subgraph**
-#   with the nodes of `graph` that are in `nodeset` and the edges of `graph`
-#   that connect those nodes.
 
 
 def network(edgeset):
     """Create a network from the given set of edges."""
     return add_edges(edgeset, null(0))
+
+# - Write a function `subgraph(nodeset, graph)` that returns the **subgraph**
+#   with the nodes of `graph` that are in `nodeset` and the edges of `graph`
+#   that connect those nodes.
 
 # ### Example networks
 #
@@ -154,8 +174,6 @@ LOOP = network({edge(1, 1)})
 K2 = network({edge(1, 2)})
 K3 = network({edge(1, 2), edge(1, 3), edge(2, 3)})
 
-# **EXERCISES:**
-#
 # - Define K1.
 # - Write a function to create a complete graph, given the number of nodes.
 #   Don't forget to write a precondition and unit tests.
@@ -167,8 +185,6 @@ C3 = network({edge(1, 2), edge(2, 3), edge(3, 1)})
 C4 = add_edges({edge(3, 4), edge(4, 1)}, delete_edges({edge(3, 1)}, C3))
 C5 = add_edges({edge(4, 5), edge(5, 1)}, delete_edges({edge(4, 1)}, C4))
 
-# **EXERCISES:**
-#
 # - Define C2.
 # - Explain how C4 is constructed from C3.
 # - Write a function to create a cycle graph, given the number of nodes.
@@ -182,8 +198,6 @@ PETERSEN = add_edges({
     # an outer pentagon (C5).
     C5)
 
-# **EXERCISES:**
-#
 # - Draw the Petersen graph on paper and label the nodes as in `PETERSEN`.
 
 # In December 1970, the Arpanet (the precursor of the Internet) had 13 nodes.
@@ -242,10 +256,13 @@ assert degree(1, LOOP) == 2
 assert degree(1, C4) == 2
 assert degree(6, PETERSEN) == 3
 
-# **EXERCISES**:
-#
 # - Explain why all nodes in a cycle graph have degree 2.
 # - What is the degree of each node in K*n*? Write some unit tests to confirm.
+# - `degree(node, graph)` assumes that `node` is in `graph`.
+#   What happens if it isn't?
+#   Consider other behaviours, like preconditions and explicit exceptions.
+# - Search SINPLE for other functions that assume the node is in the graph,
+#   and change them if necessary.
 
 
 def adjacent(node1, node2, graph):
@@ -269,8 +286,6 @@ def isolated(node, graph):
 assert isolated(1, N1)
 assert not isolated(1, K2)
 
-# **EXERCISES:**
-#
 # - Rewrite `isolated()` using `adjacent()`.
 # - Rewrite `isolated()` using `degree()`.
 # - Which of the 3 versions is more efficient?
@@ -313,6 +328,25 @@ assert simple(PETERSEN)
 assert not simple(LOOP)
 
 
+def density(graph):
+    """
+    A graph's **density** is the ratio of its actual and potential edges:
+    it's its size divided by the size of a complete graph of the same order.
+    """
+    o = order(graph)
+    return size(graph) / (o * (o - 1) / 2.0)
+
+# Every null graph has density 0.
+assert density(N2) == 0
+# Every complete graph has density 1.
+assert density(K3) == 1
+# A graph with loops can have density > 1.
+assert density(add_edges({edge(1, 1)}, K3)) > 1
+
+# - What is `density(EMPTY)`? Should it be any particular value?
+# - Explain why 2.0 is used instead of 2. Hint: look up 'division in Python'.
+
+
 def degree_sequence(graph):
     """A graph's **degree sequence** is a descending list of nodes' degrees."""
     degrees = [degree(node, graph) for node in nodes(graph)]
@@ -324,8 +358,6 @@ assert degree_sequence(N2) == [0, 0]
 assert degree_sequence(K3) == [2, 2, 2]
 assert degree_sequence(ARPANET) == [4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2]
 
-# **EXERCISES:**
-#
 # - Write two functions to compute respectively the maximum and minimum degrees
 #   of a degree sequence.
 # - Write two functions to compute respectively the maximum and minimum degrees
@@ -352,11 +384,9 @@ assert degree_distribution(EMPTY) == [0, 0]
 assert degree_distribution(N2) == [1, 0, 0, 0]
 assert degree_distribution(LOOP) == [0, 0, 1]
 
-# **EXERCISES:**
-#
 # - Explain how `degree_distribution()` works.
 #   Why is the list never longer than `order(graph) + 2`?
-# - Change `degree_distribution` so that trailing zeros are removed.
+# - Change `degree_distribution` so that the list has no trailing zeros.
 # - Write a function to compute the degree distribution from a degree sequence.
 
 
@@ -371,8 +401,6 @@ def mean_degree(graph):
 assert mean_degree(N2) == 0
 assert mean_degree(C3) == 2
 
-# **EXERCISES:**
-#
 # - Rewrite `mean_degree()` to make it much more efficient.
 #   Hint: each edge contributes 2 to the total degree count.
 
@@ -394,19 +422,17 @@ assert k_regular(EMPTY) == -1
 assert k_regular(N2) == 0
 assert k_regular(LOOP) == 2
 assert k_regular(C3) == 2
-assert k_regular(PETERSEN) == 3
 
-# **EXERCISES:**
-#
-# - Write a function to check whether a graph is **regular**,
-#   i.e. whether all its nodes have the same degree.
-# - Write a function to check whether a degree sequence forms a regular graph.
-# - Write a function to check whether a graph is a null graph.
-# - Write a function to check whether a graph is a complete graph.
-#   Hint: find a 2-regular graph with 3 nodes that is not K3, to see that
-#   just using `k_regular` is not enough.
-# - Write a function to check whether a graph is a cycle graph.
-# - Write a function to check whether a graph is **cubic**, i.e. 3-regular.
+# - Write a function to check if a graph is **regular**,
+#   i.e. if all its nodes have the same degree.
+# - Write a function to check if a degree sequence forms a regular graph.
+# - Write a function to check if a graph is a null graph.
+# - Write a function to check if a graph is **cubic**, i.e. 3-regular.
+#   The Petersen graph is cubic.
+# - Write a function to check if a graph is a complete graph.
+#   Include a unit test with a 2-regular graph of order 3 that is not K3.
+# - Write a function to check if a graph is a cycle graph.
+#   Include a unit test with a 2-regular graph of order 2 that is not C2.
 
 
 def neighbourhood(node, graph):
@@ -442,8 +468,7 @@ assert shortest_path(1, 1, LOOP) == [1]
 assert shortest_path(3, 1, K3) == [3, 1]
 assert shortest_path(1, 4, C5) == [1, 5, 4]
 
-# **EXERCISES:**
-#
+# - Explain how `shortest_path` works.
 # - Write a function to return *all* shortest paths between two nodes.
 
 
@@ -451,35 +476,37 @@ def distance(node1, node2, graph):
     """The **distance** is the length of the shortest path from node1 to node2.
 
     The length of a path is the number of its edges.
-    If there's no path, return -1 to represent infinite distance.
+    If there's no path, the distance is infinite.
     """
-    return len(shortest_path(node1, node2, graph)) - 1
+    path = shortest_path(node1, node2, graph)
+    if path == []:
+        return float("infinity")
+    else:
+        return len(path) - 1
 
-assert distance(1, 1, LOOP) == 0
+assert distance(1, 1, C3) == 0
 assert distance(2, 4, C4) == 2
 assert distance(3, 2, K3) == 1
-assert distance(1, 5, K3) == -1
+assert distance(1, 2, N2) == float("infinity")
 
 
 def diameter(graph):
     """A graph's **diameter** is the longest of all pairwise distances."""
-    diameter = -1
+    INFINITY = float("infinity")
+    diameter = -INFINITY
     for node1 in nodes(graph):
         for node2 in nodes(graph):
             d = distance(node1, node2, graph)
-            if d == -1:
-                return d
-            elif d > diameter:
+            if d > diameter:
                 diameter = d
-    return diameter
+    return diameter if diameter >= 0 else INFINITY
 
-assert diameter(EMPTY) == -1
-assert diameter(N2) == -1
+assert diameter(EMPTY) == float("infinity")
+assert diameter(N2) == float("infinity")
 assert diameter(K3) == 1
 assert diameter(C5) == 2
 
-# **EXERCISES:**
-#
+# - Explain how `diameter()` works.
 # - Rewrite `diameter()` to make it twice as fast.
 #   Hint: the distance from A to B is the distance from B to A.
 
@@ -488,9 +515,63 @@ def connected(graph):
     """A network is **connected** if every node is reachable from any node."""
     for node1 in nodes(graph):
         for node2 in nodes(graph):
-            if distance(node1, node2, graph) == -1:
+            if distance(node1, node2, graph) == float("infinity"):
                 return False
     return True
+
+assert connected(N1)
+assert not connected(N2)
+
+# - Make `connected()` more efficient for the case the graph is connected.
+# - Add a function to check if a network is resilient to single edge failure,
+#   i.e. if it remains connected after any one edge is removed.
+#   Write a unit test showing that ARPANET is resilient.
+
+
+def components(graph):
+    """A **component** is a maximal connected subgraph.
+
+    Return a list of the graph's components, in no particular order.
+    """
+    components = []
+    visited = set()
+    for node in nodes(graph):
+        component = null(0)
+        to_visit = [node]
+        while to_visit != []:
+            node = to_visit.pop(0)
+            if node not in visited:
+                visited.add(node)
+                component = add_nodes({node}, component)
+                for neighbour in neighbourhood(node, graph):
+                    component = add_edges({edge(node, neighbour)}, component)
+                    to_visit.append(neighbour)
+        if order(component) > 0:
+            components.append(component)
+    return components
+
+assert components(K3) == [K3]
+assert [order(c) for c in components(N2)] == [1, 1]
+assert [size(c) for c in components(N2)] == [0, 0]
+
+# - Explain how `components()` works. Explain its unit tests.
+# - Rewrite `components()` using the `subgraph()` function an earlier exercise.
+# - Rewrite `connected()` using `components()`.
+
+
+def giant_component(graph):
+    """A **giant component** is a component with the majority of nodes.
+
+    Return the giant component if there is one, otherwise None.
+    """
+    o = order(graph)
+    for component in components(graph):
+        if order(component) > o / 2.0:
+            return component
+    return None
+
+assert giant_component(C3) == C3
+assert giant_component(N2) is None
 
 
 # Import a random number generator with uniform distribution
@@ -513,16 +594,12 @@ assert size(random(4, 0)) == 0
 assert size(random(4, 1)) == 6
 assert simple(random(4, 0.5))
 
-# **EXERCISES:**
-#
+# - Write a function to create a simple random graph of given order and size.
 # - Write a program that imports SINPLE, creates several random graphs,
-#   collects their mean degrees and diameters, and checks whether the
+#   collects their mean degrees and diameters, and checks if the
 #   empirical values match the expected theoretical values.
 #   The expected mean degree is `n/p`.
-# - Write a function to create a simple random graph of given order and size.
 
-# ### Exercises
-#
 # 1. What is the mean degree of the empty graph?
 # 1. Write a program that imports SINPLE and reports various properties
 #    of the Arpanet network, e.g. the most connected nodes.
@@ -536,10 +613,6 @@ assert simple(random(4, 0.5))
 # Learn about default parameter values in Python and change `edge()`.
 # Add a function to return the weight of an edge.
 # Modify `shortest_path()` to return the path with the smallest total weight.
-#
-# Check that ARPANET is robust to any communication link failing, i.e.
-# the network remains connected if a single edge is removed, i.e.
-# each edge belongs to a cycle.
 #
 # Check that KARATE has two highly connected members, the student founder and
 # the instructor, who are not friends of each other,
